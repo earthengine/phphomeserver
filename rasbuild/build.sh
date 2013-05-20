0b3855d518eccb5328000a3025f1ac013739c50a
Earth Engine <earthengine@gmail.com>
2013-05-20 20:10:48 +1000

    Modified sctipts and moved files from Windows to Linux

--- rasbuild/build.sh
+++ rasbuild/build.sh
@@ -6,7 +6,7 @@ echo $ROOT
 PACKAGES="linux-raspberrypi linux-firmware raspberrypi-firmware systemd-sysvcompat less iputils procps-ng syslog-ng"
 PACKAGES="$PACKAGES man man-db grep dialog psmisc which sudo dhcpcd ifplugd net-tools netctl "
 PACKAGES="$PACKAGES openntpd openssh smbclient nano openldap aria2 parted"
-PACKAGES="$PACKAGES lighttpd fcgi php php-cgi sqlite imagemagick"
+PACKAGES="$PACKAGES lighttpd ssmtp fcgi php php-cgi sqlite imagemagick"
 CACHE=$HOME/pacman_cache
 DB=$HOME/pacman_db
 
@@ -16,14 +16,13 @@ DB_TEMP=$(mktemp -d)
 echo $DB_TEMP
 sed -e "s@/etc/pacman.d/mirrorlist@$FILES/mirrorlist@g" $FILES/pacman.conf > $DB_TEMP/pacman.conf
 CONF=$DB_TEMP/pacman.conf
-PACMAN_BASE="./pacman --root $ROOT --cachedir $CACHE --config $CONF"
+PACMAN_BASE="pacman --root $ROOT --cachedir $CACHE --config $CONF"
 PACMAN="$PACMAN_BASE --dbpath=$DB_TEMP"
-install -d $ROOT/var/lib/pacman
 $PACMAN_BASE --dbpath=$DB -Sy || return 0
 cp -R $DB/* $DB_TEMP/ || return 0
 $PACMAN -S filesystem licenses gcc-libs || return 0
 $PACMAN -S $PACKAGES || return 0
-$PACMAN -U $HOME/php-imagick-3.0.1-4-armv6h.pkg.tar.xz || return 0
+$PACMAN -U /home/joe/php-imagick-3.0.1-4-armv6h.pkg.tar.xz || return 0
 
 cp -v {$FILES,$ROOT}/boot/cmdline.txt
 cp -v {$FILES,$ROOT}/boot/config.txt
@@ -41,6 +40,7 @@ chmod 640 $ROOT/etc/shadow
 cp -v $ROOT/etc/netctl/{examples/,}ethernet-dhcp
 cp -v $FILES/etc/ssh/* $ROOT/etc/ssh
 cp -v {$FILES,$ROOT/root}/startup.sh
+cp -v {$FILES,$ROOT}/etc/systemd/system/initialsetup.service
 chmod +x $ROOT/root/startup.sh
 install -d $ROOT/home/joe
 
@@ -50,7 +50,7 @@ $PACMAN -Q > $HOME/targetlist
 chown joe $HOME/targetlist
 }
 
-losetup /dev/loop0 $HOME/archlinuxarm.img
+losetup /dev/loop0 /remote/phphomeserver/rasbuild/archlinuxarm.img
 kpartx -v -a /dev/loop0
 mkfs.vfat /dev/mapper/loop0p1
 mkfs.ext4 /dev/mapper/loop0p2
--- rasbuild/files/startup.sh
+++ rasbuild/files/startup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-if [ ! -z \$1 ]; then
-	echo "%wheel ALL=(ALL) ALL" >> \$1
+if [ ! -z $1 ]; then
+	echo "%wheel ALL=(ALL) ALL" >> $1
 else
-	export EDITOR="./\$0"
+	export EDITOR="./$0"
 	visudo
 	locale-gen
 	netctl start ethernet-dhcp
 	netctl enable ethernet-dhcp
 	systemctl enable openntpd sshd lighttpd
+	parted /dev/mmcblk0 --script mkpart primary ext4 1878MB $(parted /dev/mmcblk0 --script print | grep '^Disk' | cut -d ' ' -f 3)
+	groupadd -g 439 ldap &>/dev/null
+	useradd -u 439 -g ldap -d /var/lib/openldap -s /bin/false ldap &>/dev/null
 	rm /root/startup.sh
 	reboot
 fi
